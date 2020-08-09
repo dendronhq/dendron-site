57e83a549d7147ffcad1656200d4a398e7cac937
kevinslin <kevin@thence.io>
2020-08-09 10:18:21 -0700

    invest: generate release noes

---
+++ scripts/gen-release-notes.js
@@ -0,0 +1,62 @@
+const execa = require('execa');
+const moment = require("moment");
+const _ = require("lodash");
+const YAML = require('yamljs');
+const fs = require("fs-extra");
+
+
+async function gitLogs(repoPath) {
+    const gitTagCmd = `git describe --abbrev=0 --tags`;
+    const { stdout: gitTag } = await execa.command(gitTagCmd, { cwd: repoPath, shell: true })
+    console.log(gitTag)
+    // v0.5.13
+    const [major, minor, patch] = gitTag.split(".");
+    const gitTagAnchor = `${major}.${minor}.0`;
+    console.log(gitTagAnchor)
+    const command = `git log ${gitTagAnchor}..HEAD --format=\'%C(auto) %h %s\'`;
+    const out = await execa.command(command, { cwd: repoPath, shell: true })
+    const commits = out.stdout.split("\n").map(line => {
+        const [skip, chash, ctype, ...cmsg] = line.split(" ")
+        return { chash, ctype, cmsg: cmsg.join(" ") }
+    })
+    const tag = { major, minor, patch }
+    return { commits, tag };
+}
+
+async function cleanCommits(commits) {
+    return _.reject(commits, ent => {
+        return _.some(["chore", "docs", "spike", "tests", "refactor", "invest"], bad => ent.ctype.startsWith(bad))
+    });
+}
+
+function addExtraFields(commits) {
+    return commits.map(c => {
+        return { ...c, topic: "", desc: "", tags: [] }
+    })
+}
+
+async function main() {
+    const stats = {
+        numCommits: 0
+    };
+    const { commits, tag } = await gitLogs('/Users/kevinlin/projects/dendronv2/dendron');
+    stats.numCommits = commits.length;
+    const commitsClean = await cleanCommits(commits);
+    const yamlString = YAML.stringify(addExtraFields(commitsClean), 4);
+    console.log(commitsClean)
+    const tagName = [tag.major, tag.minor, tag.patch].join(".");
+    fs.writeFileSync(`data/${tagName}.yml`, yamlString)
+}
+
+const sections = {
+    commands: {},
+    pods: {},
+    schemas: {},
+    notes: {},
+    fixes: {},
+    enhancements: {},
+    workspace: {},
+    other: {},
+}
+
+main();
--- vault/dendron.template.release.md
+++ vault/dendron.template.release.md
@@ -6,7 +6,7 @@ updated: 1596374871110
 created: 1596374871110
 ---
 
-# 2020-07-25 Release Notes (version 0.4)
+# {{date}} Release Notes (version {{version}})
 
 {{note}}
 
@@ -18,22 +18,6 @@ These release notes are summary of the more notable changes, for the full list, 
 
 ## Features
 
-### New Getting Started Experience ([dd4f50e](https://github.com/dendronhq/dendron/commit/dd4f50eb169e7f9686c4e3fbabca3b2a6c1e1bb7))
-
-![](https://foundation-prod-assetspublic53c57cce-8cpvgjldwysl.s3-us-west-2.amazonaws.com/assets/images/dendron-quickstart.gif)
-
-Dendron has a completely re-done getting started. you can see it by running `> Dendron: Initialize Workspace` in a new workspace and then navigating to `dendron.quickstart`
-
-## Fixes
-
-## Enhancements
-
-## Next
-
 ## Thank You
 
 Last and most of all, a big **thanks** to all these people that contributed issues and suggestions during this release.
-
-- [Alexis Argyris](https://github.com/alexisargyris)
-- [Jay A. Patel](https://github.com/jayp)
-- [Kiran Pathakota](https://github.com/kpathakota)
